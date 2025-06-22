e0443ea567c5940f27c25221f084816ab6f6c1b6
taruntelitech <taruntelitech@gmail.com>
2025-06-22 14:20:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Placeholder for future interactivity
+console.log("Inkoratee website loaded.");
+
+function toggleMenu() {
+  const nav = document.getElementById("mobileNav");
+  nav.classList.toggle("show");
+}
